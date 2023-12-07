d25db89ed0ffa69c7edb4c93625698a0b181bac8
Gustavo <ghferreira92@gmail.com>
2023-12-07 14:06:21 -0300

    capturando eventos do teclado

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ function ArmazenaMemoria() {
     memoria += parseFloat(visor.value) || 0;
 }
 
+function SubtraiMemoria() {
+    memoria -= parseFloat(visor.value) || 0;
+}
+
 function RecuperaMemoria() {
     visor.value += memoria;
 }
@@ -28,3 +32,44 @@ function RecuperaMemoria() {
 function LimpaMemoria() {
     memoria = 0;
 }
+
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            AddVisor(event.key);
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            AddVisor(event.key);
+            break;
+        case 'w':
+            ArmazenaMemoria();
+            break;
+        case 'e':
+            SubtraiMemoria();
+            break;
+        case 'r':
+            RecuperaMemoria();
+            break;
+        case 'Enter':
+            calcularResultado();
+            break;
+        case 'c':
+            limparVisor();
+            break;
+        case 'm':
+            LimpaMemoria();
+            break;
+    }
+});
